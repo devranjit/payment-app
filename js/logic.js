cf24cfbc1329db63d6ad29939306af75b9407f4a
devranjit <dvranjitplays@gmail.com>
2024-09-20 12:02:47 +0600

    add money function added

--- js/logic.js
+++ js/logic.js
@@ -2,9 +2,34 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
 event.preventDefault();
 
 
-const addedMoney =  document.getElementById('input-add-money');value;
+const addedMoney =  document.getElementById('input-add-money').value;
 
-const pinNumber =  document.getElementById('input-pin-number');value;
+const pinNumber =  document.getElementById('input-pin-number').value;
+
+
+if(pinNumber === '1234'){
+
+    const balance = document.getElementById('current-balance').innerText;
+
+    const balanceNumber = parseFloat(balance);
+
+    const newMoney = parseFloat(addedMoney);
+
+    const finalBalance = balanceNumber + newMoney ;
+
+
+    console.log(finalBalance);
+
+     document.getElementById('current-balance').innerText = finalBalance;
+
+
+
+}
+
+else{
+    console.log("something is wrong , please try again later ");
+}
+
 
 
 
