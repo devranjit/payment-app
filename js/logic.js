f0538f9c9cfa99bfdd46bba39125530c554c22a7
devranjit <dvranjitplays@gmail.com>
2024-09-20 10:47:40 +0600

    payment app live

---
+++ js/logic.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+event.preventDefault();
+
+
+const addedMoney =  document.getElementById('input-add-money');value;
+
+const pinNumber =  document.getElementById('input-pin-number');value;
+
+
+
+
+})
